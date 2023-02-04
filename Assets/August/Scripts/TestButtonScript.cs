fb9728b26b5f8ce82210df39ac00f35cb743050b
augustjsmith <123837178+augustjsmith@users.noreply.github.com>
2023-02-03 23:40:13 -0600

    Text Manager fleshed out.
    Timer and sound added, buttons and example scene for all in-game text aside from clan descriptions.

--- Assets/August/Scripts/TestButtonScript.cs
+++ Assets/August/Scripts/TestButtonScript.cs
@@ -6,15 +6,4 @@ public class TestButtonScript : MonoBehaviour
 {
     public DialogueText dialogueText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
