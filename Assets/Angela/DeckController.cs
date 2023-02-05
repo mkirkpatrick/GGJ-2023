c7562834b360c2a67066b1c2ff35d72e155ef770
Aawalters <113148805+Aawalters@users.noreply.github.com>
2023-02-05 11:12:58 -0600

    deckcontroller update for enemy functionality

--- Assets/Angela/DeckController.cs
+++ Assets/Angela/DeckController.cs
@@ -24,10 +24,11 @@ public class DeckController : MonoBehaviour
     public Deck GetNewDeck(Player _player)
     {
         Deck temp = new Deck();
-        for(var i=0; i<allCards.Count; i++){
+        for(var i = 0; i < allCards.Count; i++){
             DeckAdd(allCards[i], temp);
         }
 
+        //adds player's clan card
         if(_player.currentClan.clanName.Equals("Huma")){
             DeckAdd(clanCards[0], temp);
         } else if (_player.currentClan.clanName.Equals("Mani")){
@@ -42,23 +43,11 @@ public class DeckController : MonoBehaviour
 
     public Deck GetEnemyDeck(Enemy e){
         Deck temp = new Deck();
+        for(var i = 0; i<e.enemyMoves.Count; i++){
+            DeckAdd(e.enemyMoves[i], temp);
+        }
+        PopulateDraw(temp);
         return temp;
-    }
-
-    //for debugging, probably won't be used
-    public void PrintDeck(){
-        Debug.Log("drawpile");
-        foreach(Card c in currentDeck.drawPile){
-            Debug.Log(c.cardName);
-        }
-        Debug.Log("hand");
-        foreach(Card c in currentDeck.hand){
-            Debug.Log(c.cardName);
-        }
-        Debug.Log("discard");
-        foreach(Card c in currentDeck.discardPile){
-            Debug.Log(c.cardName);
-        }
     }
 
     //shuffles cards
@@ -72,7 +61,7 @@ public class DeckController : MonoBehaviour
         shuffledCards.AddRange(deck.drawPile);
         //iterates through and shuffles the cards
         
-        for(var i=0; i<shuffledCards.Count; i++){
+        for(var i = 0; i < shuffledCards.Count; i++){
             int rand = Random.Range(i, shuffledCards.Count);
             var temp = shuffledCards[i];
             shuffledCards[i] = shuffledCards[rand];
@@ -89,11 +78,19 @@ public class DeckController : MonoBehaviour
 
     //takes card from top of pile, removes and puts in the hand
     public Card DrawCard(Deck deck){
+        if(deck.drawPile.Count <= 0){
+            Shuffle(false, deck);
+        }
         Card c = deck.drawPile[0];
+
+        //debugging
         Debug.Log("Drawing Card..." + c.cardName);
+
         deck.hand.Add(c);
         deck.drawPile.RemoveAt(0);
-        //update the combo index
+
+        //if this is buggy just comment out, its for the root system
+        updateComboIndex(deck);
         return c;
     }
 
@@ -123,12 +120,12 @@ public class DeckController : MonoBehaviour
     }
 
     //calculates the range of the root combo (returns the furthest index)
-    public int updateComboIndex(Deck deck){
+    public void updateComboIndex(Deck deck){
         CardType c = deck.hand[0].cardType;
         bool isCombo = true;
         var index = 1;
         var comboIndex = 0;
-        while((index<5)&&isCombo){
+        while((index<5) && isCombo){
             if(deck.hand[index].cardType.Equals(c)){
                 index++;
                 comboIndex = index;
@@ -136,7 +133,10 @@ public class DeckController : MonoBehaviour
                 isCombo = false;
             }
         }
-        return comboIndex;
+        this.comboIndex = comboIndex;
     }
 
+    public Card getEnemyMove(Deck enemyDeck){
+        return enemyDeck.hand[Random.Range(0, 5)];
+    }
 }
