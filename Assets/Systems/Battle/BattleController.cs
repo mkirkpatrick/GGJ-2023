bd7034e0e5c24605574e9773bc2c31be901a6d0a
Conner A <eraserlark@gmail.com>
2023-02-04 22:13:41 -0500

    BattleController cont'd

--- Assets/Systems/Battle/BattleController.cs
+++ Assets/Systems/Battle/BattleController.cs
@@ -6,19 +6,48 @@ public class BattleController : MonoBehaviour
 {
     private DeckController deckController;
     private PlayerController playerController;
+    private Enemy enemy;
     public BattleView battleView;
     public HandView handView;
 
     private void Start()
     {
         deckController = GameController.instance.deckController;
+        enemy = new Enemy();
 
-        
-       
+        Deck currentDeck = deckController.GetNewDeck();
+        deckController.Shuffle(false, currentDeck);
+
+        //handView.cardSlots[0].card...
 
         // Load Player - health
         // Load Enemy
 
         //
+    }
+
+    void PlayerTurn()
+    {
+
+    }
+
+    void EnemyTurn()
+    {
+
+    }
+
+    void CardTurn()
+    {
+        //For when deck is up, player is choosing card
+    }
+
+    void Victory()
+    {
+
+    }
+
+    void Defeat()
+    {
+
     }
 }
