3ff352f5ce96f0a9db7ac5160f45179e29692aad
mkirkpatrick <mtraviskirkpatrick@gmail.com>
2023-02-05 14:11:38 -0600

    Player/Enemy Image Loading

--- Assets/Systems/Battle/BattleView.cs
+++ Assets/Systems/Battle/BattleView.cs
@@ -11,6 +11,10 @@ public class BattleView : MonoBehaviour
     public TMP_Text playerHealth;
     public TMP_Text enemeyHealth;
 
+    public Image playerImage;
+    public List<Sprite> beetles;
+    public Image enemyImage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +24,9 @@ public class BattleView : MonoBehaviour
     public void UpdateView(Player _player, Enemy _enemy) {
         SetPlayerHealth(_player.healthCurrent, _player.healthMax);
         SetEnemyHealth(_enemy.healthCurrent, _enemy.healthMax);
+
+        SetPlayerImage(_player);
+        SetEnemyImage(_enemy);
     }
 
     public void SetPlayerHealth(int _healthCurrent, int _healthMax) {
@@ -28,5 +35,24 @@ public class BattleView : MonoBehaviour
     public void SetEnemyHealth(int _healthCurrent, int _healthMax)
     {
         enemeyHealth.text = _healthCurrent.ToString() + " / " + _healthMax.ToString();
+    }
+
+    public void SetPlayerImage(Player _player) {
+        switch (_player.currentClan.clanName)
+        {
+            case "Huma":
+                playerImage.sprite = beetles[0];
+                break;
+            case "Mani":
+                playerImage.sprite = beetles[1];
+                break;
+            case "Nih-Tee":
+                playerImage.sprite = beetles[2];
+                break;
+        }
+    }
+    public void SetEnemyImage(Enemy _enemy)
+    {
+        enemyImage.sprite = _enemy.sprite;
     }
 }
