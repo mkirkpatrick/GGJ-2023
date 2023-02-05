12791844330dff9ac2c8ec16e2213e3b67fd6b53
augustjsmith <123837178+augustjsmith@users.noreply.github.com>
2023-02-04 18:50:22 -0600

    Game Over Scene Exists, Is Appropriately Forlorn

--- Assets/August/Scripts/GameOverScript.cs
+++ Assets/August/Scripts/GameOverScript.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntroCinematicScript : MonoBehaviour
+public class GameOverScript : MonoBehaviour
 {
     public GameObject textManager;
     public TextManager textManagerScript;
     public DialogueText dialogueText;
+    public AudioSource audioSource;
 
     void Start()
     {
         textManagerScript = textManager.GetComponent<TextManager>();
+        audioSource = GetComponent<AudioSource>();
         dialogueText = GetComponent<DialogueText>();
         StartCoroutine(PauseThenRun());
     }
 
     IEnumerator PauseThenRun()
     {
+        audioSource.Play();
         yield return new WaitForSeconds(2f);
         textManagerScript.StartDialogue(dialogueText);
     }
-
 }
