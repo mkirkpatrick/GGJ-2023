471424aca12c34a7ba801791c20400c0b9d97738
Conner A <eraserlark@gmail.com>
2023-02-04 22:04:17 -0500

    Starting BattleController

--- Assets/Systems/Battle/BattleController.cs
+++ Assets/Systems/Battle/BattleController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class BattleController : MonoBehaviour
 {
     private DeckController deckController;
+    private PlayerController playerController;
+    public BattleView battleView;
+    public HandView handView;
 
     private void Start()
     {
         deckController = GameController.instance.deckController;
+
+        
+       
 
         // Load Player - health
         // Load Enemy
